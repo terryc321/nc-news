d728d4311c84c242f8e0f29d16f08225a9006ff3
terry <terry.cadd@gmail.com>
2022-11-01 11:00:38 +0000

    added react-bootstrap cards

--- src/Article.js
+++ src/Article.js
@@ -1,10 +1,21 @@
 import './Article.css';
 
-function Article(props) {
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const Article = (props) => {
     const {article} = props;
-    return (
-            <div className="article">{article.title}</div>
-    );
+    
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+          <Card.Title>{article.title}</Card.Title>
+          <Card.Text>{article.body}</Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Article;
--- src/ArticleList.js
+++ src/ArticleList.js
@@ -1,6 +1,12 @@
 import './ArticleList.css';
 import Article from './Article';
 
+
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
 import { useState, useEffect } from "react";
 
 const ArticleList = (props) =>  {
@@ -21,16 +27,22 @@ const ArticleList = (props) =>  {
     
     if (isLoading) return <h3>Loading Articles...</h3>;
     
-    const articleList = articles.map((article, index) => {
-        return <Article key={index} article={article} />
-    });
+        return (
+            <Row xs={1} md={4} className="g-4">
+            {articles.map((article, idx) => (
+                    <Col>
+                    <Card bg='success'>
+                    <Card.Body>
+                    <Card.Title>{article.title}</Card.Title>
+                    <Card.Text></Card.Text>
+                    </Card.Body>
+                    </Card>
+                    </Col>
+            ))}
+            </Row>
+        );
 
-    return (
-            <div>
-            <h2>List of Available Articles</h2>
-            <div className="articleList">{ articleList } </div>      
-            </div>
-    );
+    
 };
 
 export default ArticleList;
